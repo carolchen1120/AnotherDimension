861f45f5805f3710f58c1c982b703d3c263f054a
carolchen1120 <72897491+carolchen1120@users.noreply.github.com>
2020-10-24 17:34:14 -0400

    Add files via upload

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,4 +1,5 @@
 public class ArrayOps {
+
   public static int sum(int[] arr) {
     int sum = 0;
     for (int i = 0; i < arr.length; i++) {
@@ -6,4 +7,16 @@ public class ArrayOps {
     }
     return sum;
   }
+
+
+  public static int largest(int[] arr) {
+    int answer = 0;
+    for (int i = 0; i < arr.length; i++) {
+        if (arr[i] > answer) {
+            answer = arr[i];
+        }
+    }
+    return answer;
+  }
+
 }
--- Tester.java
+++ Tester.java
@@ -1,19 +1,39 @@
 public class Tester {
   public static void main(String[] args) {
     ArrayOps myAnswers = new ArrayOps();
+
+    // --- The following are to test sum() ---
+    // int[] myArray1 = {0, 1, 2, 3, 4, 5};
+    // System.out.println(myAnswers.sum(myArray1) == 15);
+    //
+    // int[] myArray2 = {0, 1, 2, 3, 4, 5, 6};
+    // System.out.println(myAnswers.sum(myArray2) == 21);
+    //
+    // int[] myArray3 = {0, 1, 2, 3, 4, 5, -5};
+    // System.out.println(myAnswers.sum(myArray3) == 10);
+    //
+    // int[] myArray4 = {-10};
+    // System.out.println(myAnswers.sum(myArray4) == -10);
+    //
+    // int[] myArray5 = {};
+    // System.out.println(myAnswers.sum(myArray5) == 0);
+
+
+    // --- The following are to test largest() ---
     int[] myArray1 = {0, 1, 2, 3, 4, 5};
-    System.out.println(myAnswers.sum(myArray1) == 15);
+    System.out.println(myAnswers.largest(myArray1) == 5);
 
-    int[] myArray2 = {0, 1, 2, 3, 4, 5, 6};
-    System.out.println(myAnswers.sum(myArray2) == 21);
+    int[] myArray2 = {10, 73, 2};
+    System.out.println(myAnswers.largest(myArray2) == 73);
 
-    int[] myArray3 = {0, 1, 2, 3, 4, 5, -5};
-    System.out.println(myAnswers.sum(myArray3) == 10);
+    int[] myArray3 = {};
+    System.out.println(myAnswers.largest(myArray3) == 0);
 
-    int[] myArray4 = {-10};
-    System.out.println(myAnswers.sum(myArray4) == -10);
+    int[] myArray4 = {-10, 10, 12, 5};
+    System.out.println(myAnswers.largest(myArray4) == 12);
 
-    int[] myArray5 = {};
-    System.out.println(myAnswers.sum(myArray5) == 0);
+    int[] myArray5 = {-10};
+    System.out.println(myAnswers.largest(myArray5) == -10);
+
   }
 }
